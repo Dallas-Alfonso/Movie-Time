e94634e3450c3e52db8e23da08ecc0d0d2ec5c29
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2021-08-16 21:25:24 -0400

    Restrucred back to functional component.  Using Rafael Code to learn from.  Thank youu

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,269 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from "firebase";
-  
-  
-class App extends React.Component {
-	state = {
-		like: 0,
-		dislike: 0,
-		likeActive: true,
-		dislikeActive: true
-	  };
-	
-	  setDislike() {
-		this.setState({
-		  dislikeActive: !this.state.dislikeActive,
-		  dislike: this.state.dislike + 1 
+
+/***************************************************************************************
+*    Title: LikesCounter
+*    Author: Rafael Castellanos-Welsh
+*    Date: 2021
+*    Code version: 2.0
+*    Availability: https://github.com/rafawelsh/MovieAPI_Project/blob/main/src/components/LikesCounter/LikesCounter.js
+*
+***************************************************************************************/
+
+
+function Popup({ selected, closePopup, imdbID }) {
+	const [likes, setLikes] = useState(0);
+	const [dislikes, setDislikes] = useState(0);
+	const [movieData, setMovieData] = useState({});
+
+	useEffect(() => {
+		if (!imdbID) return null;
+		// retrieve likes/dislikes from Firebase and set data
+		// ELSE set a new entry in Firebase with movieID and set data
+		const movieRef = firebase.database().ref(`${imdbID}/`);
+		movieRef.on("value", (data) => {
+			if (data.val() !== null) {
+				setMovieData({
+					...data.val(),
+				});
+			} else {
+				firebase.database().ref(`${imdbID}/`).set({
+					likes: 0,
+					dislikes: 0,
+				});
+			}
 		});
-	  }
-	  setLike() {
-		this.setState({
-		  likeActive: !this.state.likeActive,
-		  like: this.state.like + 1
-		});
-	  }
-	
-	  handleLike() {
-		if (this.state.dislikeActive) {
-		  this.setLike();
-		}
-		this.setLike();
-	  }
-	
-	  handleDislike() {
-		if (this.state.likeActive) {
-		  this.setDislike();
-		  
-		}
-		this.setDislike();
-	  }
-	
+	}, [imdbID]);
 
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 		  quantity: 1,
-// 		  show: true,
-// 		  max: 5,
-// 		  min: 0
-// 		};
-// 	  }
+	useEffect(() => {
+		if (!movieData) return null;
+		const { dislikes: down, likes: up } = movieData;
+		setDislikes(down);
+		setLikes(up);
+	}, [dislikes, likes, movieData]);
 
-//   IncrementItem = () => {
-// 	this.setState(prevState => {
-// 	  if(prevState.quantity < 9) {
-// 		return {
-// 		  quantity: prevState.quantity + 1
-// 		}
-// 	  } else {
-// 		return null;
-// 	  }
-// 	});
-// }
-// DecreaseItem = () => {
-//   this.setState(prevState => {
-// 	if(prevState.quantity > 0) {
-// 	  return {
-// 		quantity: prevState.quantity - 1
-// 	  }
-// 	} else {
-// 	  return null;
-// 	}
-//   });
-// }
-// handleChange = (event) => {
-//     this.setState({quantity: event.target.value});
-//   }
-	
+	const handleLikeClick = () => {
+		const updates = {};
+		updates[`${imdbID}/likes`] = firebase.database.ServerValue.increment(1);
+		firebase.database().ref().update(updates);
+	};
 
-	// constructor(props) {
-	// 	super(props)
-	// 	this.state = {
-	// 	  counter: 0
-	// 	}
-	// 	this.handleIncrement = this.handleIncrement.bind(this);
-	// 	this.handleDecrement = this.handleDecrement.bind(this);
+	const handleDislikeClick = (e) => {
+		const updates = {};
+		updates[`${imdbID}/dislikes`] = firebase.database.ServerValue.increment(1);
+		firebase.database().ref().update(updates);
+	};
 
-	//   }
-
-	//   incrementMe = () => {
-	// 	var newCount = this.state.coun
-	// 	function like () {
-	// 		if (newCount !== 0) {
-	// 		  return newCount+=1
-	// 		}
-	// 		else {
-	// 		  return 1
-	// 		}
-	// 	}
-	// 	this.setState({
-	// 	  count: like()
-	//    })
-	// }
-
-	// decrementMe = () => {
-	// 	var newCount = this.state.count
-	// 	function dislike () {
-	// 	  if (newCount !== 0) {
-	// 		return newCount-=1
-	// 	  }
-	// 	  else {
-	// 	   return -1
-	// 	 }
-	//    }
-	// 	this.setState({
-	// 	  count: dislike()
-	// 	})
-	//  }
-
-  
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       counter: 0
-//     }
-//     this.myFunction = this.myFunction.bind(this);
-//   }
-  
-//   myFunction(e) {
-//     alert("The value of counter is " + this.state.counter)
-//     this.setState({ counter: this.state.counter + 1 })
-//   }
-
-// myFunction = () => {
-// 	let newCount = this.state.count + 1
-//     this.setState({
-//       count: newCount
-//     })
-// }
-
-	//  handleIncrement = () => {
-	// 	this.setState(prevCount => prevCount + 1);
-	//   };
-	  
-	//  handleDecrement = () => {
-	// 	this.setState(prevCount => prevCount - 1);
-	//   };
+	console.log({ likes }, { dislikes });
 
 
-  render() {
-	  const {selected, closePopup} = this.props;
-	  const firebaseApp = firebase.apps[0];
-	  function LikeTally({}){
-		  const [womp, setwomp] =  (0);
-	  }
-    return (
-	
-	<section className="popup">
-		<div className="content">
-			<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
-			
+	return (
+		<section className="popup">
+			<div className="content">
+				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
+
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<p>{selected.Director}</p>
 				<div className="plot">
+
 					<img src={selected.Poster} alt=''/>
+
 					<p>{selected.Plot}</p>
+
 				</div>
+
 				<button className="close" onClick={closePopup}>Close</button>
+			</div>
 
+			{/* Voting Portion */}
 
-				<div >
-				
-					<div class="voting">
-						<button  onClick={() => this.handleLike()} id="likebtn"  >
-							<i class="fas fa-thumbs-up"></i>
-							<h5>your count is{this.state.like}</h5>
-						</button>
-						<button  onClick={() => this.handleDislike()} id="dislikebtn" className={({ ["active"]: this.state.dislikeActive })}>
-							<i class="fas fa-thumbs-down">your count is{this.state.dislike}</i>
-						</button>
-						<code>
-						 <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
-						</code>
-					</div>
+			<div class="voting">
 
-
-					{/* <p>Hello From GFG</p>
-					<button onClick={this.myFunction}>Click me!</button> */}
-				</div>
-		</div>
-	</section>
-	);
-}
-}
-  
-export default App;
-
+				<button onClick={() => handleLikeClick(imdbID)} className='counter'>
+					{/* <i class="fas fa-thumbs-up"></i> */}👍 {likes}
+				</button>
+					{/* <input type="number" id="input1" value="0"name=""></input> */}
+				<button onClick={() => handleDislikeClick(imdbID)} className='counter'>
+					{/* <i class="fas fa-thumbs-down"></i> */}👎 {dislikes}
+				</button>
+					{/* <input id="dislikeCount" type="number" id="input2" value="0"name=""></input> */}
+					
+			</div>
 			
+		</section>
+	)
+}
 
-// function Popup({ selected, closePopup }) {
-
-// 	const [count, setCount] = useState(0);
-
-// 	// const handleIncrement = () => {
-// 	// 	setCount(prevCount => prevCount + 1);
-// 	//   };
-	  
-// 	// const handleDecrement = () => {
-// 	// 	setCount(prevCount => prevCount - 1);
-// 	//   };
-
-// 	return (
-// 		<section className="popup">
-// 			<div className="content">
-// 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
-				
-// 				<p className="rating">Rating: {selected.imdbRating}</p>
-// 				<p>{selected.Director}</p>
-// 				<div className="plot">
-				
-// 					<img src={selected.Poster} alt=''/>
-					
-// 					<p>{selected.Plot}</p>
-					
-// 				</div>
-				
-// 				<button className="close" onClick={closePopup}>Close</button>
-				
-		
-// 				{/* onClick={handleIncrement} */}
-// 				{/* onClick={handleDecrement} */}
-// 			 {/* Method 1 */}
-// 			<div class="voting">
-// 				<button onClick={() =>setCount(count + 1)} id="likebtn" >
-// 					<i class="fas fa-thumbs-up"></i>
-// 					<h5>your count is {count}</h5>
-// 				</button>
-					
-// 				<button onClick={() =>setCount(count - 1)} id="dislikebtn">
-// 					<i class="fas fa-thumbs-down">your count is {count}</i>
-// 				</button>
-					
-// 			</div>
-
-			 
-
-		
-// 			</div>
-// 		</section>
-// 		)
-// 	}
-		
-// export default Popup
-
-
-
-{/* <div class="wrapper">
-			<div class="like">
-				<i  onclick ={handleIncrement} class="fas fa-thumbs-up"></i>
-				<h5>count is {count}</h5>
-				
-				<br></br>
-				<input type="number" id="likeCount" value="0" name=""></input>
-			</div>
-			<div class="dislike">
-				<i class="fas fa-thumbs-down"></i>
-				<br></br>
-				<input type="number" id="dislikeCount" value="0" name=""></input>
-			</div>
-			</div>  */}
+export default Popup 
